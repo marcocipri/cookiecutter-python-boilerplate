fcde25c0f70a24b2e8fe55422663c7e64457b054
Konstantin Tretyakov <kt@ut.ee>
2014-03-21 17:41:40 +0200

    Fix

--- {{cookiecutter.project}}/src/{{cookiecutter.egg}}/setup.py
+++ {{cookiecutter.project}}/src/{{cookiecutter.egg}}/setup.py
@@ -26,22 +26,22 @@ class PyTest(TestCommand):
 
 version = "{{cookiecutter.version}}"
 
-setup(name="{{cookiecutter.project}}",
+setup(name={{cookiecutter.project.__repr__()}},
       version=version,
-      description="{{cookiecutter.description or ''}}",
+      description={{cookiecutter.description.__repr__()}},
       long_description=open("README.rst").read(),
       classifiers=[ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 1 - Planning',
         'Programming Language :: Python'
       ],
-      keywords="{{cookiecutter.keywords}}", # Separate with spaces
-      author="{{cookiecutter.author}}",
-      author_email="{{cookiecutter.author_email}}",
-      url="{{cookiecutter.url}}",
-      license="{{cookiecutter.license_name}}",
+      keywords={{cookiecutter.keywords.__repr__()}}, # Separate with spaces
+      author={{cookiecutter.author.__repr__()}},
+      author_email={{cookiecutter.author_email.__repr__()}},
+      url={{cookiecutter.url.__repr__()}},
+      license={{cookiecutter.license_name.__repr__()}},
       packages=find_packages(exclude=['examples', 'tests']),
       include_package_data=True,
-      zip_safe={{cookiecutter.zip_safe}},
+      zip_safe={{cookiecutter.zip_safe.__repr__()}},
       tests_require=['pytest'],
       cmdclass={'test': PyTest},
       
